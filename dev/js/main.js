7586145e59599fb698c9c7e411093961743b4672
Elena <elent15.job@yandex.ru>
2023-12-18 17:53:42 +0300

    Update files, add about-us.html, wholesale.html

--- dev/js/main.js
+++ dev/js/main.js
@@ -272,3 +272,22 @@ new Swiper('.offers__swiper', {
     }
   }
 });
+
+const video = () => {
+  const videoBtn = document.querySelector('.video__play-btn');
+
+  if (videoBtn) {
+    videoBtn.addEventListener('click', () => {
+      const videoContainer = videoBtn.closest('.video__container');
+      const videoBg = videoContainer.querySelector('.video__bg')
+      const videoContent = videoContainer.querySelector('.video__content')
+      const src = videoContent.dataset.src;
+
+      videoBtn.classList.add('video__play-btn--hidden');
+      videoBg.remove();
+      videoContent.insertAdjacentHTML('afterbegin', '<iframe class="video__iframe" src="' + src + '" frameborder="0" allow="autoplay;" allowfullscreen></iframe>');
+    });
+  }
+}
+
+video();
